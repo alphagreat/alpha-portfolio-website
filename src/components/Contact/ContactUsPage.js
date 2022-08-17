4eae860a3562ff4e67f175b26cda4c0078b582c3
danielbontii <bontiidaniel97@gmail.com>
2022-08-17 20:00:26 +0000

    added email

--- src/components/Contact/ContactUsPage.js
+++ src/components/Contact/ContactUsPage.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ContactUsPage.css";
-import phone from "../../images/phone.png";
 
 import Footer from "../Footer/Footer";
 import NavbarComponent from "../Navbar/NavbarComponent";
@@ -9,7 +8,83 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+import emailjs from "@emailjs/browser";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import isEmail from "validator/lib/isEmail";
+
 function ContactUsPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [msg, setMsg] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [error, setError] = useState("");
+
+  const clearInputs = () => {
+    setName("");
+    setEmail("");
+    setMsg("");
+  };
+
+  const hideError = () => {
+    setTimeout(() => setError(""), 2000);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setError("name");
+      setErrorMessage("Please provide a name");
+      hideError();
+      return;
+    }
+
+    if (!email) {
+      setError("email");
+      setErrorMessage("Please enter your email");
+      hideError();
+      return;
+    }
+
+    if (!isEmail(email)) {
+      setError("invalid");
+      setErrorMessage("Please add a valid email");
+      hideError();
+      return;
+    }
+
+    if (!msg) {
+      setError("body");
+      setErrorMessage("Please add a message");
+      hideError();
+      return;
+    }
+
+    emailjs
+      .send(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: name,
+          message: msg,
+          sender_email: email,
+          sender_name: name,
+        },
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then((_) => {
+        toast.success("Email Sent!");
+      })
+      .catch((_) => {
+        toast.error("Something went wrong");
+      });
+
+    clearInputs();
+  };
   return (
     <Container fluid>
       <Container>
@@ -40,12 +115,40 @@ function ContactUsPage() {
           </p>
           <h2>Let’s start a Conversation</h2>
           <form>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Message" />
-            <button>Send</button>
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            {error && error === "name" && (
+              <p className="Contact-error"> {errorMessage} </p>
+            )}
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {error && error === "email" && (
+              <p className="Contact-error"> {errorMessage} </p>
+            )}
+            {error && error === "invalid" && (
+              <p className="Contact-error"> {errorMessage} </p>
+            )}
+            <input
+              type="text"
+              placeholder="Message"
+              value={msg}
+              onChange={(e) => setMsg(e.target.value)}
+            />
+            {error && error === "body" && (
+              <p className="Contact-error"> {errorMessage} </p>
+            )}
+            <button onClick={(e) => handleClick(e)}>Send</button>
           </form>
         </section>
+        <ToastContainer />
       </Container>
       <Footer />
     </Container>
--- src/components/Navbar/NavbarComponent.js
+++ src/components/Navbar/NavbarComponent.js
@@ -7,24 +7,37 @@ import Container from "react-bootstrap/Container";
 
 function NavbarComponent() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="dark" fixed="top">
       <Container>
-        <Navbar.Brand href="/">alphaGreat</Navbar.Brand>
+        <Navbar.Brand href="/" className="linkText">
+          alphaGreat
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto mr-10">
-            <NavDropdown title="Operations" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Alpha Tech</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+          <Nav className="ms-auto">
+            <NavDropdown
+              title="Operations"
+              id="collasible-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1" className="linkText">
+                Alpha Tech
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2" className="linkText">
                 Alpha Academia
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Alpha Pi</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item href="#action/3.3" className="linkText">
+                Alpha Pi
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4" className="linkText">
                 Alpha Home Tuition
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/about-us">About Us</Nav.Link>
-            <Nav.Link href="/contact-us">Contact Us</Nav.Link>
+            <Nav.Link href="/about-us" className="linkText">
+              About Us
+            </Nav.Link>
+            <Nav.Link href="/contact-us" className="linkText">
+              Contact Us
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
